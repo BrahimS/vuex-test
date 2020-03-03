137005fd4bc4db47ad3ddf45dd85fa300ac4bff9
BS <bs@bss-mac-pro.home>
2020-03-03 10:43:51 +0100

    Add bootstrap and jquery as well as popperjs

---
+++ vuex-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <nav class="navbar navbar-expand-lg navbar-light shadow mb-4">
+    <div class="container">
+      <router-link to="/">ToDos</router-link> 
+      <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto mt-2">
+      </ul>
+      <router-link to="/about">About</router-link>
+      </div>
+    </div>
+    </nav>
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.shadow{
+  box-shadow: 0 .15rem 1.75rem 0 rgba(58,59,69, .15) !important;
+}
+</style>
